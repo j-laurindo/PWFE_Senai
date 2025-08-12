00682a28f09a3d6e2f7c306e04d43c046be29b0a
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-08-12 16:53:07 -0300

    aula 12/08

---
+++ JS/Exercicios/Aula12_08/exercicio4.js
@@ -0,0 +1,7 @@
+let n = prompt("Digite um número: ");
+let i = 1;
+
+while(i <= n){
+    console.log(i);
+    i++;
+}
---
+++ JS/Exercicios/Aula12_08/scriptDOM.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("button");
+
+function random(num){
+    return Math.floor(Math.random() * (num + 1));
+}
+
+btn.onclick = function(){
+    let cor =
+    "rgb(" + random(255) + "," + random(255) 
+    + "," + random(255) + ")";
+    document.body.style.backgroundColor = cor;
+}
