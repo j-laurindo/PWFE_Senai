ad0a3e64d89781aea6e7b1b2cb85360798d21982
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-08-19 16:56:06 -0300

    Aula 19-08 | Atividade Bingo

--- JS/Exercicios/Aula12_08/scriptDOM.js
+++ JS/Exercicios/Aula12_08/scriptDOM.js
@@ -4,9 +4,25 @@ function random(num){
     return Math.floor(Math.random() * (num + 1));
 }
 
-btn.onclick = function(){
+btn.addEventListener("click", () => {
     let cor =
     "rgb(" + random(255) + "," + random(255) 
     + "," + random(255) + ")";
     document.body.style.backgroundColor = cor;
-}
+});
+
+// function bgChange(event){
+//     let cor =
+//     "rgb(" + random(255) + "," + random(255) 
+//     + "," + random(255) + ")";
+
+//     event.target.style.backgroundColor = cor
+// }
+
+// btn.addEventListener("click", bgChange)
+  
+let ele = document.querySelector("h1");
+let novo = document.createElement("h1");
+ele.appendChild(novo);
+novo.innerText
+console.log(ele.innerHTML);
