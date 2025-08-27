dfb1ead1aa66234cc70324e5e706b943e3550d62
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-08-27 13:39:46 -0300

    Delete JS/Exercicios/Aula19_08/AtividadeBingo directory

--- JS/Exercicios/Aula19_08/AtividadeBingo/script.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector("button");
-
-function random(num){
-    return Math.floor(Math.random() * (75 - 1 + 1) + 1);
-}
-
-btn.addEventListener("click", () => {
-    let 
-})
